df00fdb6c4b71485df355d16bf759adcf2f017d5
ekbhast <ekbhast@gmail.com>
2024-03-20 14:45:53 +0300

    add Loop and verification data

--- js/script.js
+++ js/script.js
@@ -23,23 +23,64 @@
 
 Проверить, чтобы все работало без ошибок в консоли */
 
-const numberOfFilms = +prompt('Сколько фильмов вы посмотрели?','');
+/* Задание на урок:
+
+1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
+
+2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
+отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит - 
+возвращаем пользователя к вопросам опять
+
+3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
+"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше - 
+"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
+
+4) Потренироваться и переписать цикл еще двумя способами*/
 
 const personalMovieDB = {
-	count:numberOfFilms,
+	count:0,
 	movies: {},
 	actor: {},
 	genres: [],
 	privat: false
 };
 
-let a = prompt('Один из последних просмотренных фильмов?',''),
-	b = +prompt ('На сколько оцените его?',''),
-	c = prompt('Один из последних просмотренных фильмов?',''),
-	d = +prompt ('На сколько оцените его?','');
+for (let i = 0; i<1; i++){
+	personalMovieDB.count  = +prompt('Сколько фильмов вы посмотрели?','');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+	if (personalMovieDB.count===0 || isNaN(personalMovieDB.count)){
+		alert('Необходимо вставить число!');
+		i--;
+	}
+}
+
+if (personalMovieDB.count < 10) {
+	alert('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count > 10 && personalMovieDB.count < 30){
+	alert('Вы классический зритель');
+} else {
+	alert('Вы киноман');
+}
 
 
+for (let i = 0; i < personalMovieDB.count; i++){
+
+	let a;
+	for (let i = 0; i<1; i++){		
+		a = prompt('Один из последних просмотренных фильмов?','Введите название фильма');
+		console.log(a);		
+		if (a === 0 || a === null || a.length > 20){
+			alert('Необходимо ввести название фильма не длиннее 20 символов!');
+			i--;
+		}
+	}
+
+	for (let i = 0; i<1; i++) {
+		personalMovieDB.movies[a] = +prompt ('На сколько оцените его?','');
+		if (personalMovieDB.movies[a]===0 || isNaN(personalMovieDB.movies[a])){
+			alert('Необходимо вставить число!');
+			i--;
+		}
+	}
+}
 console.log (personalMovieDB);
